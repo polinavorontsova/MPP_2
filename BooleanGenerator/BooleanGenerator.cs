d9e80aa20b38997cd55ac7fa49c6f2e7fff8efd0
polina.vorontsova <polina.vorontsova.21@mail.ru>
2021-10-23 18:54:47 +0300

    Added boolean and datetime generator in different assemblies

---
+++ BooleanGenerator/BooleanGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Core.Entities;
+using Core.Services;
+
+namespace BooleanGenerator
+{
+    public class BooleanGenerator : IValueGenerator
+    {
+        public object Generate(GeneratorContext context)
+        {
+            return context.Random.Next(1 << 1) == 1;
+        }
+
+        public bool CanGenerate(Type type)
+        {
+            return type == typeof(bool);
+        }
+    }
+}
---
+++ DateGenerator/DateGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Core.Entities;
+using Core.Services;
+
+namespace DateGenerator
+{
+    public class DateGenerator : IValueGenerator
+    {
+        public object Generate(GeneratorContext context)
+        {
+            return DateTime.FromBinary(context.Random.Next());
+        }
+
+        public bool CanGenerate(Type type)
+        {
+            return type == typeof(DateTime);
+        }
+    }
+}
