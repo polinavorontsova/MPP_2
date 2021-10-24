6762eab173f12b8b45a1edbc9b290a1543043e0b
polina.vorontsova <polina.vorontsova.21@mail.ru>
2021-10-24 11:29:57 +0300

    Added reflection utility class

---
+++ Core/Utilities/ReflectionUtilities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Core.Utilities
+{
+    public class ReflectionUtilities
+    {
+        private ReflectionUtilities()
+        {
+        }
+
+        public static object GetDefaultValue(Type t)
+        {
+            return t.IsValueType ? Activator.CreateInstance(t) : null;
+        }
+    }
+}
