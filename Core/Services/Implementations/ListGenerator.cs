299fb120c359d60aa1b7d49930b6455324641d44
polina.vorontsova <polina.vorontsova.21@mail.ru>
2021-10-25 21:34:02 +0300

    Added list generator

---
+++ Core/Services/Implementations/ListGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Core.Entities;
+
+namespace Core.Services.Implementations
+{
+    public class ListGenerator : IValueGenerator
+    {
+        public object Generate(GeneratorContext context)
+        {
+            var listInnerType = context.TargetType.GetGenericArguments().First();
+            var list = (IList) typeof(List<>)
+                .MakeGenericType(listInnerType)
+                .GetConstructor(Type.EmptyTypes)?
+                .Invoke(null);
+
+            var method = context.Faker.GetType()
+                .GetMethod("Create", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            var listSize = context.Random.Next(1 << 3, 1 << 6);
+            for (var i = 0; i < listSize; i++) list.Add(method.Invoke(context.Faker, new object[] {listInnerType}));
+
+            return list;
+        }
+
+        public bool CanGenerate(Type type)
+        {
+            return type.IsGenericType &&
+                   type.GetGenericTypeDefinition().IsAssignableFrom(typeof(List<>));
+        }
+    }
+}
